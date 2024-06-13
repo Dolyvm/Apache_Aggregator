e81a566f01b2e9a79ef585631eee5b0ee6f78bdb
Dolyvm <114623065+Dolyvm@users.noreply.github.com>
2024-06-13 21:52:41 +0300

    Update config.py

--- config.py
+++ config.py
@@ -4,5 +4,6 @@ import os
 class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://gentelman:your_password@localhost:5432/postgres')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    LOG_DIR = os.getenv('LOG_DIR', 'D:/Server/Bin/Apache24/logs')
+     #Поменять на свои пути
+    LOG_DIR = os.getenv('LOG_DIR', 'Apache24/logs')
     LOG_MASK = os.getenv('LOG_MASK', 'access.log*')
